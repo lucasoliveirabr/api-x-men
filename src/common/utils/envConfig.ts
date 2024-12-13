2c953ad58ea61740f8bb2034c6f124a4f3140c92
lucasoliveirabr <lucas.oliveirabr@outlook.com>
2024-12-13 12:09:48 +0000

    build: remove dotenv and update package-lock.json

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -1,7 +1,4 @@
-import dotenv from "dotenv";
 import { cleanEnv, host, num, port, str, testOnly } from "envalid";
-
-dotenv.config();
 
 export const env = cleanEnv(process.env, {
   NODE_ENV: str({ devDefault: testOnly("test"), choices: ["development", "production", "test"] }),
